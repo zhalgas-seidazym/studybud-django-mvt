c0bfc84d9c4699a54cc8d8ec7911fe37e9146dde
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-02-16 14:46:43 +0500

    added delete comment

--- baseapp/views.py
+++ baseapp/views.py
@@ -75,16 +75,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id = pk)
     room_messages = room.message_set.all().order_by('-created')
-
+    participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
             user = request.user,
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk = room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'baseapp/room.html', context)
 
 @login_required(login_url='login')
@@ -128,4 +129,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'baseapp/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id = pk)
+
+    if request.user != message.user:
+        return HttpResponse("Access Denied")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'baseapp/delete.html', {'obj': message})
 

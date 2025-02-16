1997d01fcb770c2cd5e9f2cfe787e6c781b6cb6c
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-02-16 14:33:47 +0500

    added comments section to room

--- baseapp/views.py
+++ baseapp/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -74,8 +74,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
-    print(room)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk = room.id)
+
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'baseapp/room.html', context)
 
 @login_required(login_url='login')

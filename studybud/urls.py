71baff8052e180d102758812f256d8bb790b3e17
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-03-04 16:13:21 +0500

    changed project name

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for learn_django project.
+URL configuration for studybud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -7,7 +7,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-based viewsx
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
